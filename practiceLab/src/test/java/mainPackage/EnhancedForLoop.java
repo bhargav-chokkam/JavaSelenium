afdaf46f99531dedfaf094c14963263c71849c5d
bhargav <bhargav@bhargav>
2022-03-15 22:31:18 +0530

    -

--- practiceLab/src/test/java/mainPackage/EnhancedForLoop.java
+++ practiceLab/src/test/java/mainPackage/EnhancedForLoop.java
@@ -1,21 +1,30 @@
 package mainPackage;
 
 public class EnhancedForLoop {
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		EnhancedForLoop obj = new EnhancedForLoop();
 		obj.singleDiamention();
-		
+		obj.multiDiemention();
 
 	}
-	public void singleDiamention()
-	{
-		int nums[] = {1,2,3};
-		for (int i : nums) 
-		{
-			System.out.print(i+" ");			
+
+	public void singleDiamention() {
+		int nums[] = { 1, 2, 3 };
+		for (int i : nums) {
+			System.out.println(i + " ");
+		}
+	}
+
+	public void multiDiemention() {
+		System.out.println("Multi Diamention Array");
+		int sampleNumbers[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		for (int i[] : sampleNumbers) {
+			for (int j : i) {
+				System.out.print(j);
+			}
+			System.out.println(" ");
+
 		}
 	}
 
 }
-
