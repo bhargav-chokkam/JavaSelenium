4083e44531ea288e26dd9773de8e3e79cdceb4e2
bhargav <bhargav@bhargav>
2022-03-15 18:12:39 +0530

    -

---
+++ practiceLab/src/test/java/mainPackage/ArrayDemo.java
@@ -0,0 +1,14 @@
+package mainPackage;
+/*
+ * In Java, Array's are Objects
+ * ArrayIndexOutOfBoundsException : The exception
+ */
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
