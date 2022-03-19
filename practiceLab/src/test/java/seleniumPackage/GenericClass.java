1c205d5b1f5e8a1009a1c5bea54fae5e019bdab5
bhargav <bhargav@bhargav>
2022-03-19 22:09:49 +0530

    -

--- practiceLab/src/test/java/seleniumPackage/GenericClass.java
+++ practiceLab/src/test/java/seleniumPackage/GenericClass.java
@@ -1,8 +1,15 @@
 package seleniumPackage;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class GenericClass {
 	WebDriver driver;
@@ -45,21 +52,11 @@ public class GenericClass {
 		case "xpath":
 			driver.findElement(By.xpath(element)).sendKeys(elementData);
 			break;
-		case "id":
-			driver.findElement(By.id(element)).sendKeys(elementData);
-			break;
-		case "classname":
-			driver.findElement(By.className(element)).sendKeys(elementData);
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(element)).sendKeys(elementData);
 			break;
-		case "name":
-			driver.findElement(By.name(element)).sendKeys(elementData);
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
 	}
@@ -69,22 +66,11 @@ public class GenericClass {
 		case "xpath":
 			driver.findElement(By.xpath(element)).clear();
 			break;
-		case "id":
-			driver.findElement(By.id(element)).clear();
-			break;
-		case "classname":
-			driver.findElement(By.className(element)).clear();
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(element)).clear();
 			break;
-
-		case "name":
-			driver.findElement(By.name(element)).clear();
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
 	}
@@ -94,120 +80,102 @@ public class GenericClass {
 		case "xpath":
 			driver.findElement(By.xpath(element)).click();
 			break;
-		case "id":
-			driver.findElement(By.id(element)).click();
-			break;
-		case "classname":
-			driver.findElement(By.className(elementType)).click();
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(elementType)).click();
 			break;
-
-		case "name":
-			driver.findElement(By.name(element)).click();
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
 	}
+
 	public void submitButton(String elementType, String element) {
 		switch (elementType.toLowerCase()) {
 		case "xpath":
 			driver.findElement(By.xpath(element)).submit();
 			break;
-		case "id":
-			driver.findElement(By.id(element)).submit();
-			break;
-		case "classname":
-			driver.findElement(By.className(element)).submit();
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(element)).submit();
 			break;
-
-		case "name":
-			driver.findElement(By.name(element)).submit();
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
 	}
+
 	public void isDisplayedCheck(String elementType, String element) {
 		switch (elementType.toLowerCase()) {
 		case "xpath":
 			driver.findElement(By.xpath(element)).isDisplayed();
 			break;
-		case "id":
-			driver.findElement(By.id(element)).isDisplayed();
-			break;
-		case "classname":
-			driver.findElement(By.className(element)).isDisplayed();
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(element)).isDisplayed();
 			break;
-
-		case "name":
-			driver.findElement(By.name(element)).isDisplayed();
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
 	}
+
 	public void isEnabledCheck(String elementType, String element) {
 		switch (elementType.toLowerCase()) {
 		case "xpath":
 			driver.findElement(By.xpath(element)).isEnabled();
 			break;
-		case "id":
-			driver.findElement(By.id(element)).isEnabled();
-			break;
-		case "classname":
-			driver.findElement(By.className(element)).isEnabled();
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(element)).isEnabled();
 			break;
-
-		case "name":
-			driver.findElement(By.name(element)).isEnabled();
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
 	}
+
 	public void isSelectedCheck(String elementType, String element) {
 		switch (elementType.toLowerCase()) {
 		case "xpath":
 			driver.findElement(By.xpath(element)).isSelected();
 			break;
-		case "id":
-			driver.findElement(By.id(element)).isSelected();
-			break;
-		case "classname":
-			driver.findElement(By.className(element)).isSelected();
-			break;
 		case "cssselector":
 			driver.findElement(By.cssSelector(element)).isSelected();
 			break;
-
-		case "name":
-			driver.findElement(By.name(element)).isSelected();
-			break;
-
 		default:
-			System.out.println("XPath, Id, ClassName, Name and cssSelector are only Supported");
+			System.out.println("XPath and cssSelector are only Supported");
 			break;
 		}
+	}
+
+	public void explicitTimeout(String elementType, String element, int waitTime) {
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(waitTime));
+		switch (elementType.toLowerCase()) {
+		case "xpath":
+			wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(element))));
+			break;
+		case "cssselector":
+			wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(element))));
+			break;
+		default:
+			System.out.println("XPath and cssSelector are only Supported");
+			break;
+		}
+
+	}
+
+	public void fluentWait(String elementType, String element, int waitTime) {
+		Wait wait = new FluentWait(driver).withTimeout(Duration.ofSeconds(waitTime)).pollingEvery(Duration.ofSeconds(2))
+				.ignoring(NoSuchElementException.class);
+		switch (elementType.toLowerCase()) {
+		case "xpath":
+			wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(element))));
+			break;
+		case "cssselector":
+			wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(element))));
+			break;
+		default:
+			System.out.println("XPath and cssSelector are only Supported");
+			break;
+		}
+
 	}
 
 }
